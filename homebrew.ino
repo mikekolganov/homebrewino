49513f8f9fb40931c4d81b5a2b09e3471b67c7ef
Mike Kolganov <mike.kolganov@gmail.com>
2017-09-25 23:47:57 +0600

    Recipe parsing

--- homebrew.ino
+++ homebrew.ino
@@ -1,35 +1,42 @@
 // RECIPES
-
-const char RECIPE_0[] PROGMEM = "CHECH|54*20|65*15|72*30|78*10|100*60(TRADITION-1;ZHATETSKIY-55)";
-const char RECIPE_1[] PROGMEM = "HOBGOBLIN|68*60|78*5|100*60(CASCADE;1)(GOLDINGS-30;FUGGLE-55)";
-const char RECIPE_2[] PROGMEM = "ZHIGULEVSKOE|54*20|64*15|72*20|78*10|100*90(ISTRA-20;MOSKOW-85)";
+// Syntax: <NAME>|<PAUSE TEMP>*<PAUSE DURATION>(<REMINDER NAME>-<MINUTES SINCE PAUSE START>)
+// Notes:
+// * Mash pauses are delimited by "|", arbitrary amount of mash pauses, must be terminated with "|"
+// * Reminders are optional and specified in round brackets at end of mash pause definition
+const char RECIPE_1[] PROGMEM = "CHECH|54*20|65*15|72*30|78*10|100*60(TRADITION@1;ZHATETSKIY@55)";
+const char RECIPE_2[] PROGMEM = "HOBGOBLIN|68*60|78*5|100*60(CASCADE@1)(GOLDINGS@30;FUGGLE@55)";
+const char RECIPE_3[] PROGMEM = "ZHIGULEVSKOE|54*20|64*15|72*20|78*10|100*90(ISTRA@20;MOSKOW@85)";
 
 const int RECIPES_COUNT = 3;
 
 const char* const RECIPES[] PROGMEM = {
-  RECIPE_0,
   RECIPE_1,
-  RECIPE_2
+  RECIPE_2,
+  RECIPE_3
 };
 
-// INCLUDES
+const char SEGMENTS_DELIMITER = '|';
+const char SEGMENT_VALUES_DELIMITER = '*';
+const char REMINDERS_DELIMITER = ';';
+const char REMINDER_VALUES_DELIMITER = '@';
+const char REMINDER_BEGIN = '(';
+const char REMINDER_END = ')';
 
+// INCLUDES
 #include <LiquidCrystal.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #include <EEPROMex.h>
 
 // PINS
+#define D_PIN_ONE_WIRE  12 // Temp sensors
+#define D_PIN_BACKLIGHT 10 // Display backlight control
+#define D_PIN_BUZZER    11 // Piezo buzzer
 
-#define D_PIN_ONE_WIRE  12
-#define D_PIN_BACKLIGHT 10
-#define D_PIN_BUZZER 11
-
-#define A_PIN_KEYS 0
-#define A_PIN_ESCAPE 1
+#define A_PIN_KEYS 0   // Arrow keys
+#define A_PIN_ESCAPE 1 // Reset key, used as Esc
 
 // UI SCREENS
-
 #define SCREEN_WELCOME            1
 #define SCREEN_MAIN_MENU          2
 #define SCREEN_CHOOSE_RECIPE_MENU 3
@@ -40,30 +47,27 @@ const char* const RECIPES[] PROGMEM = {
 #define SCREEN_MANUAL_BREWING     8
 
 // UI DELAYS
-
 #define WELCOME_DELAY         300
 #define SENSORS_DELAY         1000
 #define RENDER_DELAY          200
 #define BACKLIGHT_DELAY       200
 #define BUTTONS_DELAY         200
-#define PERSIST_BREWING_DELAY 60000 // Every minute
+#define PERSIST_BREWING_DELAY 60000 // Every minute, save current brewing state for recovering after unexpected power off / hanging
 
 // SETTINGS
-
 #define SETTING_ADDRESS_HEATER_POWER 100
 #define SETTING_ADDRESS_TANK_VOLUME  102
 #define SETTING_ADDRESS_BACKLIGHT    104
 
-#define SETTING_HEATER_POWER_DEFALUT 2000; // Watt
-#define SETTING_TANK_VOLUME_DEFALUT  25;   // Liters
-#define SETTING_BACKLIGHT_DEFALUT    80;   // Percents
+#define SETTING_HEATER_POWER_DEFALUT 2000 // Watt
+#define SETTING_TANK_VOLUME_DEFALUT  25   // Liters
+#define SETTING_BACKLIGHT_DEFALUT    80   // Percents
 
 int SETTING_HEATER_POWER;
 int SETTING_TANK_VOLUME;
 int SETTING_BACKLIGHT;
 
 // ENVIRONMENT
-
 OneWire oneWire(D_PIN_ONE_WIRE);
 DallasTemperature sensors(&oneWire);
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
@@ -71,13 +75,11 @@ LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 float THERMOMETER_ONE;
 float THERMOMETER_TWO;
 
-// CHARACTER CONSTANTS
-
+// SYMBOLS
 const char POINTER_SYMBOL = char(165);
 const char DEGREE_SYMBOL = char(223);
 
-// BREWING!
-
+// BREWING
 #define BREWING_MODE_MANUAL 1
 #define BREWING_MODE_RECIPE 2
 
@@ -85,7 +87,7 @@ boolean      BREWING_WORK_IN_PROGRESS = false;
 
 byte         BREWING_MODE;
 unsigned int BREWING_PASSED;
-boolean      BREWING_PAUSED = false;
+boolean      BREWING_ON_HOLD = false;
 
 int          BREWING_MANUAL_TEMP;
 int          BREWING_MANUAL_TIME;
@@ -93,13 +95,11 @@ int          BREWING_MANUAL_TIME;
 String       BREWING_RECIPE;
 
 // UI STATE
-
 int ACTIVE_SCREEN     = 1;
 int ACTIVE_ITEM_INDEX = 0;
 int PREVIOUS_ACTIVE_ITEM_INDEX = 0;
 
 // INPUT STATE
-
 boolean BUTTON_UP     = false;
 boolean BUTTON_RIGHT  = false;
 boolean BUTTON_DOWN   = false;
@@ -109,10 +109,10 @@ boolean BUTTON_ESCAPE = false;
 
 // LOOP THROTTLE
 
-unsigned long fnDelay_render = 0;
+unsigned long fnDelay_render              = 0;
 unsigned long fnDelay_requestTemperatures = 0;
 unsigned long fnDelay_requestButtonsState = 0;
-unsigned long fnDelay_adjustBacklight = 0;
+unsigned long fnDelay_adjustBacklight     = 0;
 unsigned long fnDelay_persistBrewingState = 0;
 
 int freeRam () {
@@ -264,33 +264,107 @@ void renderMainMenu() {
   }
 }
 
-String getRecipeName(String recipeCode) {
-  return recipeCode.substring(0, recipeCode.indexOf('|'));
+String getRecipeName(String recipe) {
+  return recipe.substring(0, recipe.indexOf(SEGMENTS_DELIMITER));
 }
 
-int getRecipeSegmentsCount(String recipeCode) {
+int getSegmentsCount(String recipe) {
   int segmentsCount = 0;
-  for (int i = 0; i < recipeCode.length(); i++) {
-    if (recipeCode.charAt(i) == '|') {
+  for (int i = 0; i < recipe.length(); i++) {
+    if (recipe.charAt(i) == SEGMENT_VALUES_DELIMITER) {
       segmentsCount += 1;
     }
   }
   return segmentsCount;
 }
 
-int getRecipeSegmentDuration(String recipeCode, int segmentIndex) {
-
+int getSegmentBegin(String recipe, int segmentIndex) {
+  int segmentCounter = 0;
+  int startPosition = recipe.indexOf(SEGMENTS_DELIMITER);
+  while (startPosition != -1) {
+    if (segmentCounter == segmentIndex) return startPosition + 1;
+    startPosition = recipe.indexOf(SEGMENTS_DELIMITER, startPosition + 1);
+    segmentCounter++;
+  }
 }
 
-int getRecipeSegmentTemperature(String recipeCode, int segmentIndex) {
-
+int getSegmentEnd(String recipe, int segmentIndex) {
+  int segmentCounter = getSegmentsCount(recipe) - 1;
+  int endPosition = recipe.length();
+  while (segmentCounter >= 0) {
+    if (segmentCounter == segmentIndex) return endPosition;
+    endPosition = recipe.lastIndexOf(SEGMENTS_DELIMITER, getSegmentBegin(recipe, segmentCounter));
+    segmentCounter--;
+  }
 }
 
-void getSegmentReminders(String recipeCode, int segmentIndex) {
-
+String getSegment(String recipe, int index) {
+  return recipe.substring(getSegmentBegin(recipe, index), getSegmentEnd(recipe, index));
 }
 
-// vvv MAGIC GOES HERE vvv
+int getSegmentDuration(String segment) {
+  int from = segment.indexOf(SEGMENT_VALUES_DELIMITER) + 1;
+  int to = segment.indexOf(REMINDER_BEGIN);
+  if (to == -1) {
+    to = segment.length();
+  }
+  return segment.substring(from, to).toInt();
+}
+
+int getSegmentTemperature(String segment) {
+  int from = 0;
+  int to = segment.indexOf(SEGMENT_VALUES_DELIMITER);
+  return segment.substring(from, to).toInt();
+}
+
+int getRemindersCount(String segment) {
+  if (segment.indexOf(REMINDER_BEGIN) == -1 || segment.indexOf(REMINDER_END) == -1) {
+    return 0;
+  }
+  int reminderCounter = 0;
+  int delimiterPosition = 0;
+  while (delimiterPosition != -1) {
+    reminderCounter++;
+    delimiterPosition = segment.indexOf(REMINDERS_DELIMITER, delimiterPosition + 1);
+  }
+  return reminderCounter;
+}
+
+int getReminderBegin(String segment, int reminderIndex) {
+  int reminderCounter = 0;
+  int startPosition = segment.indexOf(REMINDER_BEGIN);
+  while (startPosition != -1) {
+    if (reminderCounter == reminderIndex) return startPosition + 1;
+    startPosition = segment.indexOf(REMINDERS_DELIMITER, startPosition + 1);
+    reminderCounter++;
+  }
+}
+
+int getReminderEnd(String segment, int reminderIndex) {
+  int reminderCounter = getRemindersCount(segment) - 1;
+  int endPosition = segment.length() - 1;
+  while (reminderCounter >= 0) {
+    if (reminderCounter == reminderIndex) return endPosition;
+    endPosition = segment.lastIndexOf(REMINDERS_DELIMITER, getReminderBegin(segment, reminderCounter));
+    reminderCounter--;
+  }
+}
+
+String getReminder(String segment, int reminderIndex) {
+  return segment.substring(getReminderBegin(segment, reminderIndex), getReminderEnd(segment, reminderIndex));
+}
+
+String getReminderName(String reminder) {
+  int from = 0;
+  int to = reminder.indexOf(REMINDER_VALUES_DELIMITER);
+  return reminder.substring(from, to);
+}
+
+int getReminderTime(String reminder) {
+  int from = reminder.indexOf(REMINDER_VALUES_DELIMITER) + 1;
+  int to = reminder.length();
+  return reminder.substring(from, to).toInt();
+}
 
 void renderRecipeBrewing() {
   lcd.clear();
@@ -306,8 +380,6 @@ void renderManualBrewing() {
   lcd.print("MANUAL BREWING");
 }
 
-// ^^^ MAGIC GOES HERE ^^^
-
 void renderChooseRecipeMenu() {
   String items[RECIPES_COUNT];
   char buffer[128];
@@ -316,7 +388,7 @@ void renderChooseRecipeMenu() {
   for (int i = 0; i < RECIPES_COUNT; i++) {
     strcpy_P(buffer, (char*)pgm_read_word(&(RECIPES[i])));
     recipe = String(buffer);
-    items[i] = getRecipeName(recipe) + "(" + getRecipeSegmentsCount(recipe) + ")";
+    items[i] = getRecipeName(recipe) + "(" + getSegmentsCount(recipe) + ")";
   }
 
   renderIterableList(items, RECIPES_COUNT);
